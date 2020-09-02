078b6d876e25799022420ccdafdd5e675f4fd48e
MAXON-BY <maxon.pro2015@gmail.com>
2020-09-02 21:41:52 +0300

    'saga...'

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,14 +1,32 @@
 import { heroItem, heroList, setPage } from './reducers/heroReducer';
-import { takeEvery } from '@redux-saga/core/effects';
+import { takeEvery, put, call } from '@redux-saga/core/effects';
 
-function* sagaWorker() {
-  // yield put('heroes/heroList');
-  // yield put('heroes/setPage');
-  // yield put('heroes/heroItem');
+function* sagaWorkerList() {
+  const payload = yield call(fetchHeroes);
+  yield put({ type: 'heroes/heroList', payload });
+}
+
+function* sagaWorkerItem() {
+  const payload = yield call(fetchHeroItem);
+  yield put({ type: 'heroes/heroItem', payload });
+}
+
+function* sagaWorkerPage() {
+  yield put({ type: 'heroes/setPage', payload });
 }
 
 export function* sagaWatcher() {
-  yield takeEvery(heroList, sagaWorker);
-  yield takeEvery(heroItem, sagaWorker);
-  yield takeEvery(setPage, sagaWorker);
+  yield takeEvery(heroList, sagaWorkerList);
+  yield takeEvery(heroItem, sagaWorkerItem);
+  yield takeEvery(setPage, sagaWorkerPage);
+}
+
+async function fetchHeroes() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`);
+  return await response.json();
+}
+
+async function fetchHeroItem() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+  return await response.json();
 }
